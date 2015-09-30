dd2bcccee81e120390d54e927f7248156ee30192
oskarth <me@oskarth.com>
2015-09-30 11:17:31 +0200

    Add better solutions for sieve.clj
    These requires further study for me to grok.

--- sieve.clj
+++ sieve.clj
@@ -13,3 +13,46 @@
       (recur (remove #(= (rem % (first coll)) 0) coll)
             (conj acc (first coll)))
       acc)))
+
+; Better solution - cgrande's version without optimizations
+; http://clj-me.cgrand.net/2009/07/30/everybody-loves-the-sieve-of-eratosthenes/
+; Need to study this more.
+(defn primes2 [max]
+  (let [enqueue (fn [sieve n factor]
+                  (let [m (+ n factor)]
+                    (if (sieve m)
+                      (recur sieve m factor)
+                      (assoc sieve m factor))))
+        next-sieve (fn [sieve candidate]
+                     (if-let [factor (sieve candidate)]
+                       (-> sieve
+                         (dissoc candidate)
+                         (enqueue candidate factor))
+                       (enqueue sieve candidate candidate)))]
+    (vals (reduce next-sieve {} (range 2 (+ max 1))))))
+
+
+; Another solution from comments (but it uses additional optimizations)
+
+; we work only with odd numbers till N
+(defn odds [n] (vec (range 3 (inc n) 2)))
+
+; it's easy to calculate index of a value in our vector of odds
+(defn odds-idx [v] (/ (- v 3) 2))
+
+; just sieve the vector from start=(index of v²)
+(defn sieve [max-idx nums v]
+  (let [start (odds-idx (* v v))]
+    (reduce #(assoc %1 %2 nil) nums (range start max-idx v))))
+
+; get primes equal or less than N
+(defn primes [n]
+  (let [max-i (Math/floor (odds-idx n))]
+    (loop [nums (odds n) i 0]
+      (if-let [v (nums i)]
+        (if (> (* v v) n)
+          (cons 2 (remove nil? nums))
+          (recur (sieve max-i nums v) (inc i)))
+        (recur nums (inc i))))))
+
+; (time (count (primes 15485863)))
