b74eb5525b69c9512aa8a4595c5ec481f6cd7e28
oskarth <me@oskarth.com>
2015-10-12 09:47:00 +0200

    Add better solution to selection sort

--- quadratic-sorts.clj
+++ quadratic-sorts.clj
@@ -54,3 +54,28 @@
     (= bubbled sorted)))
 
 (test-random-vec)
+
+;; Better solution (for selection sort)
+
+; Writing with transients first goes against the rule of transients - only use
+; them after you have correct algorithm
+
+(import 'java.util.ArrayList)
+
+(defn arr-swap! [#^ArrayList arr i j]
+  (let [t (.get arr i)]
+    (doto arr
+      (.set i (.get arr j))
+      (.set j t))))
+
+(defn sel-sort! [arr]
+  (let [n (.size arr)]
+    (letfn [(move-min! [start-i]
+              (loop [i start-i]
+                (when (< i n)
+                  (when (< (compare (.get arr i) (.get arr start-i)) 0)
+                    (arr-swap! arr start-i i))
+                  (recur (inc i)))))]
+      (doseq [start-i (range (dec n))]
+        (move-min! start-i))
+      arr)))
