a695ac104ec018e612103d669b9880cc8e49f631
oskarth <me@oskarth.com>
2015-10-13 12:36:23 +0200

    Stroke-API: Update dependencies

--- stroke-api/project.clj
+++ stroke-api/project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [http-kit "2.1.11"]
+                 [compojure "1.4.0"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-mock "0.3.0"]])
