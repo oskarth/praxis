e02199d3712684fba2710c5f183907edfc0d8803
oskarth <me@oskarth.com>
2015-10-13 12:26:10 +0200

    Init stroke-api

---
+++ stroke-api/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to stroke-api
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ stroke-api/test/stroke_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns stroke-api.core-test
+  (:require [clojure.test :refer :all]
+            [stroke-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
