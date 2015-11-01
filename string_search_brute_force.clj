0968323428851dfc2e34c8173b55ac95b99dc056
oskarth <me@oskarth.com>
2015-11-01 18:47:19 +0100

    minor work on string search

--- string_search_brute_force.clj
+++ string_search_brute_force.clj
@@ -1,18 +1,37 @@
 (ns string-brute)
 
 (defn search [pattern string]
-  (loop [pi 0 si 0]
-    (cond (= (count pattern) pi) si   ;; return index in string
-          (nil? (get string si)) false ;; no match
-          (= (get pattern pi) (get string si)) (recur (inc pi) (inc si))
-          :else (recur 0 (inc si)))))
+  (loop [pi 0 si 0 res '()]
+    (cond (= (count pattern) pi) (recur (inc pi) (inc si) (conj res si))
+          (nil? (get string si)) res
+          (= (get pattern pi) (get string si)) (recur (inc pi) (inc si) res)
+          :else (recur 0 (inc si) res))))
+
 
 
 (comment
+  ;; doing some keyboard experiments, so not a lot of code but something.
 
   (def pattern "foo")
   (def string "hello foo bar there")
 
+  (def dict (slurp "/usr/share/dict/words"))
+  (def mindict (apply str (take 1000 dict)))
+  
+  (search "aar" mindict)
+  (nth mindict 37)
+
+  (find-string 34 "")
+
+  
+  ;; what if last word? newline there too ithink
+  (defn find-string [i s]
+    (let [char (nth mindict i)]
+      (if (= char \newline) s
+          (find-string (inc i) (str s char)))))
+
+
+  
+ 
   (= (search "foo" "hello foo bar") 9)
-  (= (search "foo" "hello fo") false)
-  )
+  (= (search "foo" "hello fo") false))
