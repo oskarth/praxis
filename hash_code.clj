064d7138f36ced99f724ddac2154fb948ee7217f
oskarth <me@oskarth.com>
2016-05-21 18:32:52 +0100

    Simplify hash fn a bit

--- hash_code.clj
+++ hash_code.clj
@@ -2,14 +2,15 @@
 ;; Reasonably fast and presumably uniform?
 ;; How can we make it better / simpler?
 
-(defn hash-code [x]
-  (if (char? x) (long x)
-      (loop [hash 1 work (str x)]
-        (if-let [[curr & todo] (seq work)]
-          (recur (unchecked-add (unchecked-multiply 31 hash)
-                                (unchecked-long (hash-code curr)))
-                 todo)
-          hash))))
+(defn hash-str [s]
+  {:pre [(string? s)]}
+  (loop [n 1 cs (seq s)]
+    (if-let [c (first cs)]
+      (recur (unchecked-add (unchecked-multiply 31 n) (unchecked-int c))
+             (rest cs))
+      n)))
+
+(hash-str "foobar blitzball")
 
 ;; test, should be true
-(every? number? (map hash-code [\a 1 55 "abc" :foo [1 2 3] {:foo :bar} "The quick brown fox jumps over the lazy dog"]))
+(every? number? (map (comp hash-str str) [\a 1 55 "abc" :foo [1 2 3] {:foo :bar} "The quick brown fox jumps over the lazy dog"]))
