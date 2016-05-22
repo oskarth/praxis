2b809eda23a4d9965e256f7bc08b1d026ae25d99
oskarth <me@oskarth.com>
2016-05-22 11:47:32 +0100

    Improve hash code

--- hash_code.clj
+++ hash_code.clj
@@ -2,15 +2,14 @@
 ;; Reasonably fast and presumably uniform?
 ;; How can we make it better / simpler?
 
-(defn hash-str [s]
-  {:pre [(string? s)]}
-  (loop [n 1 cs (seq s)]
+(defn hash-code [x]
+  (loop [n 1 cs (-> x str seq)]
     (if-let [c (first cs)]
       (recur (unchecked-add (unchecked-multiply 31 n) (unchecked-int c))
              (rest cs))
       n)))
 
-(hash-str "foobar blitzball")
+(hash-code "foobar blitzball")
 
 ;; test, should be true
-(every? number? (map (comp hash-str str) [\a 1 55 "abc" :foo [1 2 3] {:foo :bar} "The quick brown fox jumps over the lazy dog"]))
+(every? number? (map hash-code [\a 1 55 "abc" :foo [1 2 3] {:foo :bar} "The quick brown fox jumps over the lazy dog"]))
